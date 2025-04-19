3394c9e7fa890690d1fba9fe0f0877b346598306
Malte <36799584+malte-kaller@users.noreply.github.com>
2025-04-19 16:29:52 +0100

    Edit copy_identify_script

--- support_scripts/copy_identify_rawbrukers.sh
+++ support_scripts/copy_identify_rawbrukers.sh
@@ -13,12 +13,16 @@ subj=$1
  echo "copying data for $subj"
 
 #Copy subject folder into created folder structure (COULD BE MODFIED TO MOVE TO AVOID DOUBLE SAVE)
-cp1=`fsl_sub -q short -l $scriptDIR/logs/move_identify cp -r $dataDIR/$subj $projectDIR/${projectname}_rawbrukers/$subj`
+    #If you want to submit this command to the cluster, use this:
+    ##cp1=`fsl_sub -q short -l $scriptDIR/logs/move_identify cp -r $dataDIR/$subj $projectDIR/${projectname}_rawbrukers/$subj`
+    #If submitted via main script wrapper
+    cp -r $dataDIR/$subj $projectDIR/${projectname}_rawbrukers/$subj
 
 #Identify the Scans in the folder and create a .txt file containing the relecant information for this scan
-
-fsl_sub -q short -l $scriptDIR/logs/move_identify -j ${cp1}  $sup_scriptDIR/identifyScan.sh $subj $projectDIR $projectname
-
+    #If you want to submit this command to the cluster, use this:
+    ##fsl_sub -q short -l $scriptDIR/logs/move_identify -j ${cp1}  $sup_scriptDIR/identifyScan.sh $subj $projectDIR $projectname
+    $sup_scriptDIR/identifyScan.sh $subj $projectDIR $projectname
+    
 echo "done for $subj"
 
 echo "Script Complete"
