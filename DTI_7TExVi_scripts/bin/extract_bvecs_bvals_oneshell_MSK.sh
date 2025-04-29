dafe8d4b40c8e44bc934d1743a12987c7ae5cd7e
Malte <36799584+malte-kaller@users.noreply.github.com>
2025-04-29 18:46:36 +0100

    adapt extract script

--- DTI_7TExVi_scripts/bin/extract_bvecs_bvals_oneshell_MSK.sh
+++ DTI_7TExVi_scripts/bin/extract_bvecs_bvals_oneshell_MSK.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-#### extract_bvecs_bvals_oneshell.sh (fixed version)
+
+# Input: $1 = folder containing method_shell1
 foldername=$1
 
-# === Extract full bvals block ===
+# === 1. Extract all bvals from method_shell1 ===
 sed -n '/PVM_DwEffBval/,/PVM_DwGradVec/{
   /PVM_DwEffBval/d
   /PVM_DwGradVec/d
   p
 }' ${foldername}/method_shell1 \
 | tr -s " " "\n" \
-| grep -E '^[0-9]+' > ${foldername}/bvals_shell1
+| grep -E '^[0-9.]+' > ${foldername}/bvals_shell1
 
-cat ${foldername}/bvals_shell1 > ${foldername}/bvals2
-cat ${foldername}/bvals2 | tr "\n" " " > ${foldername}/bvals_method
-xargs -a ${foldername}/bvals_method -n1 printf "%1.f " > ${foldername}/bvals
+# Make FSL-style bvals: 1 line, space-separated
+cat ${foldername}/bvals_shell1 | tr '\n' ' ' > ${foldername}/bvals
 
-# === Extract raw gradient vectors (DWIs only) ===
-sed -n '/##\$PVM_DwDir=(/,/##\$PVM_DwDgSwitch/{
+# === 2. Extract raw DWI vectors (gradients) ===
+sed -n '/##\\$PVM_DwDir=(/,/##\\$PVM_DwDgSwitch/{
   /PVM_DwDir/d
-  /##\$PVM_DwDgSwitch/d
+  /##\\$PVM_DwDgSwitch/d
   p
 }' ${foldername}/method_shell1 \
 | tr -s " " "\n" \
 | grep -E '^-?[0-9.]+' \
 | awk 'ORS=NR%3?" ":"\n"' > ${foldername}/bvecs_dwi_only.txt
 
-# === Dynamically match bvals to bvecs (insert 0 0 0 for b=0 volumes) ===
+# === 3. Dynamically build bvecs based on bvals ===
 bvecs_out=${foldername}/bvecs_shell1
 rm -f $bvecs_out
 
@@ -41,9 +41,9 @@ for b in $(cat ${foldername}/bvals_shell1); do
   fi
 done
 
-# === Transpose to FSL bvecs format ===
+# === 4. Transpose bvecs to FSL format ===
 cat ${foldername}/bvecs_shell1 > ${foldername}/bvecs_temp
-/vols/Data/km/cetisca/projects/diffpostproc-exvivo-mouse-bruker7t/bin/transpose_bvecs.sh ${foldername}/bvecs_temp >  ${foldername}/bvecs
+/vols/Data/km/cetisca/projects/diffpostproc-exvivo-mouse-bruker7t/bin/transpose_bvecs.sh ${foldername}/bvecs_temp > ${foldername}/bvecs
 
-# === Cleanup ===
-rm ${foldername}/bvals_shell1 ${foldername}/bvals2 ${foldername}/bvecs_temp ${foldername}/bvecs_dwi_only.txt ${foldername}/bvals_method
+# === 5. Cleanup ===
+rm ${foldername}/bvals_shell1 ${foldername}/bvecs_temp ${foldername}/bvecs_dwi_only.txt
