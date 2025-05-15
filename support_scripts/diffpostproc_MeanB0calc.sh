099c1ab6fae4d5c3542eb04b13e5120de69bcf2e
Malte <36799584+malte-kaller@users.noreply.github.com>
2025-05-15 14:15:43 +0100

    Change

--- support_scripts/diffpostproc_MeanB0calc.sh
+++ support_scripts/diffpostproc_MeanB0calc.sh
@@ -22,7 +22,7 @@ fslroi ${dtiDir}/data.nii.gz ${dtiDir}/b0_1 0 1
 fslroi ${dtiDir}/data.nii.gz ${dtiDir}/b0_2 11 1
 fslroi ${dtiDir}/data.nii.gz ${dtiDir}/b0_3 22 1
 
-fslmerge -t ${dtiDir}/b0s_data ${dtiDir}/b01.nii.gz ${dtiDir}/b02.nii.gz ${dtiDir}/b03.nii.gz
+fslmerge -t ${dtiDir}/b0s_data ${dtiDir}/b0_1.nii.gz ${dtiDir}/b0_2.nii.gz ${dtiDir}/b0_3.nii.gz
 
 # Step 2: Merge the extracted volumes into a new 4D file
 # (This step is optional for averaging just two volumes but included for completeness)
@@ -31,7 +31,7 @@ fslmerge -t $dtiDir/merged_volumes_B0.nii.gz ${dtiDir}/b0_1.nii.gz ${dtiDir}/b0_
 
 # Step 3: Average the volumes
 # Since you're only working with two volumes, you can directly average them without merging, but for demonstration:
-fslmaths $dtiDir/merged_volumes.nii.gz -Tmean $dtiDir/B0_mean.nii.gz
+fslmaths $dtiDir/merged_volumes_B0.nii.gz -Tmean $dtiDir/B0_mean.nii.gz
 
 #Adjust images accordingly
 fslmaths $dtiDir/B0_mean.nii.gz -thr 0 $dtiDir/B0_mean.nii.gz
